fec8cc5750e68caee5790200e46a0797b0648f09
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-09-18 21:19:50 +0200

    Made updated to the home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
 import { HomeMainSection } from "../components/HomeMainSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandshake } from "@fortawesome/free-solid-svg-icons";
 import "./HomePage.css";
 
 export function Home() {
@@ -14,6 +16,29 @@ export function Home() {
       </header>
       <main>
         <HomeMainSection />
+        <div className="contacts-grid">
+          <div>
+            <h3 className="pt-4">
+              <span className="me-3">
+                <FontAwesomeIcon icon={faHandshake} />
+              </span>
+              Let work together
+            </h3>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="/Contact">Let's get in touch</a>
+              </li>
+              <li className="mt-2 mb-2">
+                <a href="/Projects">Check out my work</a>
+              </li>
+              <li>
+                <a href="/">See my resume</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>

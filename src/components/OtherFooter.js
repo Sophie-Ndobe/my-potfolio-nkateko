e869b17ffde198be23b7c33730e865c441d4a763
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-12-12 13:12:28 +0200

    OtherFooter component, updated ContactPage

---
+++ src/components/OtherFooter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function OtherFooter() {
+  return <div>This is the other footer</div>;
+}
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
-import { Footer } from "../components/Footer";
+import { OtherFooter } from "../components/OtherFooter";
+import "./ContactPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export function Contact() {
   return (
     <div className="Contact">
       <NavBar />
-      contact page contact page contact page
-      <Footer />
+      <h1>Let's get in touch!</h1>
+      <div>
+        <FontAwesomeIcon icon={faEnvelope} />
+        <h2>ndobes76@gmail.com</h2>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faLinkedin} />
+        <h2>LinkedIn</h2>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faGithub} />
+        <h2>Github</h2>
+      </div>
+      <OtherFooter />
     </div>
   );
 }
